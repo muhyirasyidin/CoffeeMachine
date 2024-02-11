a45deb54b371493c40fa7ec59681dddd50732356
Muhyi Rasyidin <muhyirasyidin0@gmail.com>
2024-02-11 20:52:39 +0700

    Use module as Object as usually

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const coffeeStock = require('./state');
 
-console.log(coffeeStock);
+const makeCoffee = (type, miligrams) => {
+  if (coffeeStock[type] >= miligrams) {
+    console.log("Kopi berhasil dibuat!");
+  } else {
+    console.log("Biji kopi habis!");
+  }
+}
+
+makeCoffee("robusta", 80);
